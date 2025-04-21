af103f52833b6cd3c496fafec367ecb1933081cd
alexslramos <51198971+alexslramos@users.noreply.github.com>
2025-04-21 16:21:20 +0000

    yes

---
+++ operadores de atribuicao.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(){
+
+/*
+Atribuição simples (=)
+Atribuição com Soma (+=)
+Atribuição com Subtração (-=)
+Atribuição com Multiplicação (*=)
+Atribuição com Divisão (/=)
+*/
+
+    int numero1 = 10, numero2, resultado;
+
+    resultado = 10;
+    printf("resultado: %d\n", resultado);
+
+    //resultado = resultado + 20;
+    resultado += 20;
+    printf("resultado: %d\n", resultado);
+
+    //resultado = resultado - numero1;
+    resultado -= numero1;
+    printf("resultado: %d\n", resultado);
+
+    //resultado = resultado * 5;
+    resultado *= 5;
+    printf("resultado: %d\n", resultado);
+
+    //resultado = resultado / 2;
+    resultado /= 2;
+    printf("resultado: %d\n", resultado);
+
+
+
+
+
+
+
+}
--- operadores matematicos.c
+++ operadores matematicos.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+int main (){
+    /*
+    Soma (+)
+    Subtração (-)
+    Multiplicação (*)
+    Divisão (/)*/ 
+
+    int numero1, numero2;
+    int soma, subtracao, multiplicacao, divisao;
+
+    printf("entre com o numero1:\n");
+    scanf("%d", &numero1);
+    printf("entre com o numero2:\n");
+    scanf("%d", &numero2);
+
+    soma = numero1 + numero2;
+
+    subtracao = numero1 - numero2;
+
+    multiplicacao = numero1 * numero2;
+
+    divisao = numero1 / numero2;
+
+printf("A soma é:%d \n", soma);
+printf("A subtracao é:%d \n", subtracao);
+printf("A multiplicacao é:%d \n", multiplicacao);
+printf("A divisão é:%d \n", divisao);
+
+
+return 0;
+}
