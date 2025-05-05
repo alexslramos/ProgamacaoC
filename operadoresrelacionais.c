7bf86d4100f8d161cf646492dfe19f898ff82a48
alexslramos <51198971+alexslramos@users.noreply.github.com>
2025-05-05 18:51:29 -0300

    operadoresrelacionais.c

---
+++ operadoresrelacionais.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+/* a>b (maior que)
+a<b (menor que)
+a=b (maior ou igual a)
+a<=b(menor ou igual a)
+a==b (igual a)
+a!=b (diferente de )
+
+  int a = 10;
+  int b = 20;
+  
+}
